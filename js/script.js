d01e3e1312278bd4657dc2c07ca3153de80dcce9
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-24 13:13:23 +0800

    2 files modified, 2 added, 1 deleted

--- js/script.js
+++ js/script.js
@@ -5,22 +5,22 @@ canvas.height = 576
 
 const player = new Player()
 
+const keys = { 
+  w: {pressed: false}, 
+  a: {pressed: false}, 
+  d: {pressed: false},
+}
 function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'white'
   c.fillRect(0, 0, canvas.width, canvas.height)
+
+  player.velocity.x = 0
+  if (keys.d.pressed) player.velocity.x = 5
+  else if (keys.a.pressed) player.velocity.x = -5
 
   player.draw()
   player.update()
 }
 
 animate()
-
-window.addEventListener('keydown', (event) => {
-  console.log(event)
-  switch (event.key) {
-    case 'w':
-      player.velocity.y = -10
-      break
-  }
-})
