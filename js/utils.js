70648dbaa33d7a99af52934d0076a9d811b40dfa
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-24 15:24:24 +0800

    2 files modified

--- js/utils.js
+++ js/utils.js
@@ -13,8 +13,8 @@ Array.prototype.createObjectsFrom2D = function () {
   this.forEach((row, yIndex) => {
     row.forEach((symbol, xIndex) => {
       if (symbol === 292) {
-        
-        // Drawing collision blocks
+
+        // Creating collision blocks
         objects.push(new CollisionBlock({ 
           position: { x: xIndex * 64, y: yIndex * 64, } 
         }))
