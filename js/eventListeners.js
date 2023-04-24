791ba86636cc31ba9d41734debedaa1d513d4c2b
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-24 16:58:07 +0800

    1 file modified

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -3,7 +3,18 @@ window.addEventListener('keydown', (event) => {
   console.log(event)
   switch (event.key) {
     case 'w':
-      if (player.velocity.y === 0) player.velocity.y = -20
+      for (let i = 0; i < doors.length; i++) {
+        const door = doors[i]
+        if (
+          player.hitbox.position.x <= door.position.x + door.width && 
+          player.hitbox.position.x + player.hitbox.width >= door.position.x &&
+          player.hitbox.position.y + player.hitbox.height >= door.position.y &&
+          player.hitbox.position.y <= door.position.y + door.height
+        ) {
+          
+        }
+      }
+      if (player.velocity.y === 0) player.velocity.y = -15
       break
     case 'a':
       keys.a.pressed = true
