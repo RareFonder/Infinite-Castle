1711522849993773db9f778b369b3cb227028428
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-24 13:36:03 +0800

    2 files modified

--- js/script.js
+++ js/script.js
@@ -3,20 +3,6 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-class Sprite {
-  constructor({ position, imageSrc }) {
-    this.position = position
-    this.image = new Image()
-    this.image.onload = () => { this.loaded = true }
-    this.image.src = imageSrc
-    this.loaded = false
-  }  
-  draw() {
-    if (!this.loaded) return
-    c.drawImage(this.image, this.position.x, this.position.y)
-  }
-}
-
 const backgroundLevel1 = new Sprite({ 
   position: { x:0, y:0, }, 
   imageSrc: 'https://stackblitz.com/files/web-platform-aygtwk/github/RareFonder/Kings-and-Pigs/main/backgroundLevel1.png',
